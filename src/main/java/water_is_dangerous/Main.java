680d51a0440156153e8564831c61178a99fd7202
NULL114 <230305197@qq.com>
2023-11-03 15:11:45 +0800

    删除了一些目前暂时用不到的代码

--- src/main/java/water_is_dangerous/Main.java
+++ src/main/java/water_is_dangerous/Main.java
@@ -4,8 +4,10 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
@@ -17,11 +19,11 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class Main {
     public static final DeferredRegister<SoundEvent> SOUNDS = DeferredRegister
             .create(ForgeRegistries.SOUND_EVENTS, Util.MOD_ID);
-    public static final RegistryObject<SoundEvent> DROPLET_DROP = SOUNDS.register("item.droplet.drop",
-            () -> new SoundEvent(new ResourceLocation(Util.MOD_ID, "item.droplet.drop")));
-    public static final RegistryObject<SoundEvent> DROPLET_HIT = SOUNDS.register("item.droplet.hit",
-            () -> new SoundEvent(new ResourceLocation(Util.MOD_ID, "item.droplet.hit")));
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Util.MOD_ID);
     public static final RegistryObject<Item> DROPLET = ITEMS.register("droplet",
             () -> new Item(new Item.Properties().tab(ItemGroup.TAB_COMBAT).fireResistant().stacksTo(1)));
+    public Main() {
+        IEventBus ibus = FMLJavaModLoadingContext.get().getModEventBus();
+        EntityInit.ENTITY_TYPES.register(ibus);
+    }
 }
